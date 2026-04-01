2639d013a7a35d902aba9c12804f708da2962107
Diya Singh <diyasingh2717@gmail.com>
2026-04-01 20:26:47 +0530

    Fix config for cloud deployment

--- config.py
+++ config.py
@@ -1,14 +1,6 @@
-# config.py
-Stock_Name=input("Enter Your Stock Name: ")
-
-# Stock settings
-STOCK_SYMBOL = Stock_Name+".NS"
-PERIOD = "10y"
-INTERVAL = "1d"
-
-# Prediction settings
+STOCK_SYMBOL = 'RELIANCE.NS'
+PERIOD = '10y'
+INTERVAL = '1d'
 FUTURE_DAYS = 1
-
-# Model settings
 TEST_SIZE_RATIO = 0.2
 RANDOM_STATE = 42
